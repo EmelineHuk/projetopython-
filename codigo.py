6008ad53628ecce73ae42d0a2a0ee59e7e913d88
emeline huk <emlinehuk@gmail.com>
2025-04-09 00:04:48 -0300

    Primeiro commit: adiciona código e produtos

---
+++ codigo.py
@@ -0,0 +1,9 @@
+import pyautogui
+pyautogui.PAUSE = 1
+pyautogui.press ('win')
+pyautogui.write ('chorme')
+pyautogui.press('enter')
+
+pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+pyautogui.press('enter')
+
